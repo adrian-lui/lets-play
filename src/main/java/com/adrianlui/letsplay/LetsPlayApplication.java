7211ae315025d8d4e09d085c9efc7ceca1d70a17
Adrian Lui <113538395+adrian-lui@users.noreply.github.com>
2023-09-18 09:08:25 +0300

    Update LetsPlayApplication.java

--- src/main/java/com/adrianlui/letsplay/LetsPlayApplication.java
+++ src/main/java/com/adrianlui/letsplay/LetsPlayApplication.java
@@ -14,25 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class LetsPlayApplication {
 
 	public static void main(String[] args) {
-//		ConnectionString connectionString = new ConnectionString("mongodb+srv://luinairda:aMURb8nDJSAhrIVS@letsplay.bur2hzf.mongodb.net/?retryWrites=true&w=majority");
-//		ServerApi serverApi = ServerApi.builder()
-//				.version(ServerApiVersion.V1)
-//				.build();
-//		MongoClientSettings settings = MongoClientSettings.builder()
-//				.applyConnectionString(connectionString)
-//				.serverApi(serverApi)
-//				.build();
-//		// Create a new client and connect to the server
-//		try (MongoClient mongoClient = MongoClients.create(settings)) {
-//			try {
-//				// Send a ping to confirm a successful connection
-//				MongoDatabase database = mongoClient.getDatabase("admin");
-//				database.runCommand(new Document("ping", 1));
-//				System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-//			} catch (MongoException e) {
-//				e.printStackTrace();
-//			}
-//		}
 		SpringApplication.run(LetsPlayApplication.class, args);
 	}
 
